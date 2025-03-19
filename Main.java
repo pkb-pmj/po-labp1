7ab7b0dc4aacb90baed0c12094aa151585df5ddd
pkb-pmj <pkb.pmj@gmail.com>
2025-03-19 21:28:39 +0100

    ✨ "doing tasks", checking for equal robots

--- Main.java
+++ Main.java
@@ -16,8 +16,44 @@ class Main {
             robot.meldunek();
             if (robot instanceof RobotSkladanie) {
                 ((RobotSkladanie) robot).powitanie();
+            } else if (robot instanceof RobotDomowy) {
+                ((RobotDomowy) robot).powitanie();
             }
             System.out.println();
+        }
+
+        for (Robot robot : roboty) {
+            robot.zadanieWykonaj();
+            robot.meldunek();
+            System.out.println();
+        }
+
+        for (Robot robot : roboty) {
+            System.out.println(robot);
+            robot.meldunek();
+            if (robot instanceof RobotSkladanie) {
+                ((RobotSkladanie) robot).powitanie();
+            } else if (robot instanceof RobotDomowy) {
+                ((RobotDomowy) robot).powitanie();
+            }
+            System.out.println();
+        }
+
+        int takieSame = 0;
+        for (Robot r1 : roboty) {
+            for (Robot r2 : roboty) {
+                if (r1 == r2)
+                    continue;
+                if (r1.equals(r2)) {
+                    takieSame++;
+                    System.out.println(r1.getNazwa() + " i " + r2.getNazwa() + " są takie same");
+                }
+            }
+        }
+        if (takieSame > 0) {
+            System.out.println("Są identyczne roboty");
+        } else {
+            System.out.println("Brak identycznych robotów");
         }
     }
 }
