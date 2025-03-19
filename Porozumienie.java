e5e9931793341c171e7876b069c07bd0720504c9
pkb-pmj <pkb.pmj@gmail.com>
2025-03-19 17:52:46 +0100

    🏷️ Porozumienie & PorozumieniePlus interfaces

---
+++ Porozumienie.java
@@ -0,0 +1,7 @@
+public interface Porozumienie {
+    /**
+     * Metoda ma wypisać na konsolę jak długo robot „żyje” (do teraz), a w przypadku
+     * robotów z limitem zadań, dodatkowo ile zadań jeszcze może wykonać.
+     */
+    void meldunek();
+}
---
+++ PorozumieniePlus.java
@@ -0,0 +1,7 @@
+public interface PorozumieniePlus extends Porozumienie {
+    /**
+     * Wypisuje na konsolę tekst „<nazwa> o numerze fabrycznym <nrFabryczny> mówi
+     * witaj”, np. „Zosia Samosia o numerze fabrycznym 3 mówi witaj”.
+     */
+    void powitanie();
+}
